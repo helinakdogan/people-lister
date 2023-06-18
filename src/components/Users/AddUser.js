eb1d256c497479a7ba2253e9aded505a2c344116
Helin Akdoğan <akdoganhelin@gmail.com>
2023-06-18 21:40:26 +0300

    18.06.2023

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddUser = (props) => {
+  return (
+    <form>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text'></input>
+   
+        <label htmlFo
